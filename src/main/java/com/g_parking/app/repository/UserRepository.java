5a4730dd49b43b5f3df59671842de18eeb5ffcff
abdelmoghitakhatar <akhaabdo6@gmail.com>
2024-05-18 01:03:23 +0100

    add @Repository to repositories

--- src/main/java/com/g_parking/app/repository/UserRepository.java
+++ src/main/java/com/g_parking/app/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.g_parking.app.repository;
 
 import com.g_parking.app.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findUserEntityByEmail(String email);
--- src/main/java/com/g_parking/app/repository/VehicleRepository.java
+++ src/main/java/com/g_parking/app/repository/VehicleRepository.java
@@ -2,10 +2,12 @@ package com.g_parking.app.repository;
 
 import com.g_parking.app.domain.VehicleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface VehicleRepository extends JpaRepository<VehicleEntity, Long> {
 
     Optional<VehicleEntity> findVehicleEntityByMatricule(String matricule);
