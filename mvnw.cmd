18129ca36be4cbfe7e9821eee797612829f69a74
abdelmoghit akhatar <116843001+abdelmoghitakhatar@users.noreply.github.com>
2024-05-29 02:14:59 +0100

    change mvnw and mvnw.cmd java config (#1)

--- mvnw.cmd
+++ mvnw.cmd
@@ -21,7 +21,7 @@
 @REM Apache Maven Wrapper startup batch script, version 3.2.0
 @REM
 @REM Required ENV vars:
-@REM JAVA_HOME - location of a JDK home dir
+@REM JAVA_HOME_17 - location of a JDK home dir
 @REM
 @REM Optional ENV vars
 @REM MAVEN_BATCH_ECHO - set to 'on' to enable the echoing of the batch commands
@@ -57,22 +57,22 @@ set ERROR_CODE=0
 @setlocal
 
 @REM ==== START VALIDATION ====
-if not "%JAVA_HOME%" == "" goto OkJHome
+if not "%JAVA_HOME_17%" == "" goto OkJHome
 
 echo.
-echo Error: JAVA_HOME not found in your environment. >&2
-echo Please set the JAVA_HOME variable in your environment to match the >&2
+echo Error: JAVA_HOME_17 not found in your environment. >&2
+echo Please set the JAVA_HOME_17 variable in your environment to match the >&2
 echo location of your Java installation. >&2
 echo.
 goto error
 
 :OkJHome
-if exist "%JAVA_HOME%\bin\java.exe" goto init
+if exist "%JAVA_HOME_17%\bin\java.exe" goto init
 
 echo.
-echo Error: JAVA_HOME is set to an invalid directory. >&2
-echo JAVA_HOME = "%JAVA_HOME%" >&2
-echo Please set the JAVA_HOME variable in your environment to match the >&2
+echo Error: JAVA_HOME_17 is set to an invalid directory. >&2
+echo JAVA_HOME_17 = "%JAVA_HOME_17%" >&2
+echo Please set the JAVA_HOME_17 variable in your environment to match the >&2
 echo location of your Java installation. >&2
 echo.
 goto error
@@ -115,7 +115,7 @@ for /F "usebackq delims=" %%a in ("%MAVEN_PROJECTBASEDIR%\.mvn\jvm.config") do s
 
 :endReadAdditionalConfig
 
-SET MAVEN_JAVA_EXE="%JAVA_HOME%\bin\java.exe"
+SET MAVEN_JAVA_EXE="%JAVA_HOME_17%\bin\java.exe"
 set WRAPPER_JAR="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
 set WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
 
